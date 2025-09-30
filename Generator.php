5cb468d6c76b64621993b4be92c2f32d4ce32eda
Adrian Pol Peligrino <adrianpolpeligrino27@gmail.com>
2025-09-30 14:06:27 +0800

    ->insert('Errors/Toasts');

--- Generator.php
+++ Generator.php
@@ -11,7 +11,7 @@ $viewContent = <<<PHP
 <?php 
 \$this->layout('Layout', ['mainContent' => \$this->fetch('Layout')]);
 \$this->start('mainContent');
-\$this->insert('Errors/Toasts');
+\$this->insert('Errors/Toastss');
 ?>
 
 <!-- Add your content here to be displayed in the browser -->
